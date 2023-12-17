75b65136fcfdf222bd592a0173f2d6a6f5330176
Will Mendes <willmendesneto@gmail.com>
2023-12-17 13:35:07 -0300

    feat: adding json-server mock only when running on dev mode

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,29 @@
 import path from 'node:path';
 
 import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
+import { defineConfig, PluginOption } from 'vite';
 import jsonServer from 'vite-plugin-simple-json-server';
 
 // https://vitejs.dev/config/
-export default defineConfig(() => {
+export default defineConfig(({ mode }) => {
+  const plugins: PluginOption[] = [react()];
+
+  if (mode === 'development') {
+    plugins.push(
+      jsonServer({
+        mockDir: 'mock',
+        urlPrefixes: ['/api'],
+      }),
+    );
+  }
+
   return {
     build: {
       modulePreload: {
         polyfill: true,
       },
     },
-    plugins: [
-      react(),
-      jsonServer({
-        mockDir: 'mock',
-        urlPrefixes: ['/api'],
-      }),
-    ],
+    plugins,
     test: {
       globals: true,
       environment: 'jsdom',
